b6a205fdb3981f90f173746e322e8c387c6f82ab
Yooshikii <yoshiki@retherswap.org>
2024-07-17 13:04:48 +0200

    Update bootnodes

--- params/bootnodes_vecno.go
+++ params/bootnodes_vecno.go
@@ -5,7 +5,7 @@ package params
 var VecnoBootnodes = []string{
 	// Foundation Bootnodes
 	"enode://c788d2afcf3c1d2a8bd23e92eb0e6306712ebb6a34c8026e217c9ba05eb9d580b8dcf39859bd4ddee80434f2793b2574f0baddcc4976d979f5d6e30247476591@203.161.54.144:30303", // Dev
-	"", // Explorer
+	"enode://3ebd502effd02de6e1c4e034e145051cba90c586bc3e11db64b1d985b27bd8a294bf477d92cb067ad2d389c5233c749f348610d99b70d15d26c510e79eb7d212@66.29.155.55:30303",   // Explorer
 
 	// Communtiy Bootnodes
 	"",
