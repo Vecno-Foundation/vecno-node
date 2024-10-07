0fb348de5255fc9d207b962f6cc9c856c11e210d
Yooshikii <yoshiki@retherswap.org>
2024-10-07 23:58:03 +0200

    Add bootnode

--- params/bootnodes_vecno.go
+++ params/bootnodes_vecno.go
@@ -4,7 +4,7 @@ package params
 // reliably and availably on the Vecno network.
 var VecnoBootnodes = []string{
 	// Foundation Bootnodes
-	"", // Explorer
+	"enode://1d3dc091ef0570b1cb8512dbbd0f02d92907597a04bc931ed86c336b699b7b13b12035ed240c5af215e2e910454ceb03185cd12875ea76b02b60e3afd7f5874b@81.167.190.96:30303", // Explorer
 	"", // Dev
 
 	// Communtiy Bootnodes
